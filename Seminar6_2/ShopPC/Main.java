bdf98df86cd9daf0b6ea8d18fbea4e58326d1bb8
Алексей <129889765+Alekseiy1980@users.noreply.github.com>
2023-09-22 08:45:42 +0300

    Update Main.java

--- Seminar6_2/ShopPC/Main.java
+++ Seminar6_2/ShopPC/Main.java
@@ -22,31 +22,7 @@ public class Main {
         int count = 5;
         PC [] pc = new PC[count];
         ShopPC shopPC = new ShopPC();
-//        int pic = random.nextInt(Brend.values().length);
-//        String brend = String.valueOf(Brend.values()[pic]);
-//        pic = random.nextInt(Os.values().length);
-//        String OS = String.valueOf(Os.values()[pic]);
-//
-//        int display = random.nextInt(17 -13)+13;
-//
-//        int osy = random.nextInt(5);
-//        switch (osy) {
-//            case 0 : osy = 4; break;
-//            case 1 : osy = 8; break;
-//            case 2 : osy = 16; break;
-//            case 3 : osy = 32; break;
-//        }
-//
-//        int vCard = random.nextInt((5-1)+1)*2;
-//        int hd = random.nextInt(8);
-//        switch(hd){
-//            case 0 : hd = 128; break;
-//            case 1 : hd = 256; break;
-//            case 2 : hd = 512; break;
-//            case 3 : hd = 1; break;
-//            case 4 : hd = 2; break;
-//            case 5 : hd = 3; break;
-//        }
+
             for(int i = 0; i <count; i++)
             {
                 int pic = random.nextInt(Brend.values().length);
@@ -80,15 +56,11 @@ public class Main {
                 }
 //        System.out.println("-------------------");
 //        System.out.println(pc[2]);
-//
-//
-//
+
 //        System.out.println("-------------");
         System.out.println(shopPC);
         shopPC.filterPC();
 
-
-//           System.out.println(brend + " " + OS + " " + display + " " + osy + " " + vCard + " " + hd );
     }
 
 }
